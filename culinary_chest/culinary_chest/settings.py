6bf352ad72028716bd3947c407fdfe8c22f55fcd
malgurvik <malgurvik@gmail.com>
2024-12-12 15:28:47 +0300

    add main app, templates and static

--- culinary_chest/culinary_chest/settings.py
+++ culinary_chest/culinary_chest/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'culinary_chest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- culinary_chest/main/views.py
+++ culinary_chest/main/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {
+        "title": "Culinary Chest",
+    }
+    return render(request, 'main/index.html', context)
